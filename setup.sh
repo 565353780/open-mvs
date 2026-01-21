5ca8e315075217d53f3dbe2179b0c6385d031a81
chLi <565353780@qq.com>
2026-01-21 17:06:32 +0800

    finish compile

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@ cd ..
 git clone --depth 1 https://github.com/cnr-isti-vclab/vcglib.git
 
 conda install cmake -y
+conda install -y -c conda-forge gcc=11 gxx=11
 conda install -c conda-forge eigen opencv ceres-solver \
-  cgal boost glfw nanoflann libjxl glad -y
+  cgal boost glfw nanoflann libjxl glad openmp -y
 
 cd open-mvs
 
@@ -14,8 +15,7 @@ cd make
 cmake .. \
   -DVCG_ROOT=${PWD}/../../vcglib \
   -DOpenMVS_USE_CUDA=ON \
-  -DCMAKE_PREFIX_PATH=${CONDA_PREFIX}
+  -DCMAKE_PREFIX_PATH=${CONDA_PREFIX} \
+  -DCMAKE_LIBRARY_PATH=${CONDA_PREFIX}/lib
 
 make -j
-
-make install
